ae46653254e46f50e20ee8b22b8becf6e9df1365
¨Jenny¨ <jgong10278@ufide.ac.cr>
2023-10-03 14:15:50 -0600

    Creando repositorio

---
+++ src/main/java/com/CENOTE_tarea1/CenoteTarea1Application.java
@@ -0,0 +1,13 @@
+package com.CENOTE_tarea1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenoteTarea1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenoteTarea1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/CENOTE_tarea1/CenoteTarea1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CENOTE_tarea1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenoteTarea1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
